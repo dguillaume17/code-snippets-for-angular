8c8b394dd54e99d1d1ce5354a54ebdd40447a5b8
GDER <g.debraine@easi.net>
2023-12-15 21:32:32 +0100

    test2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, ChangeDetectorRef, Component, NgZone } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     template: `
-        <h1>Hello World</h1>
+        <h1>Hello World {{getCounter}}</h1>
     `
 })
-export class AppComponent {}
+export class AppComponent {
+
+    counter = 0;
+
+    public get getCounter(): number {
+        console.log('call')
+        return this.counter
+    }
+
+    constructor(
+        private _ngZone: NgZone,
+        private _cd: ChangeDetectorRef
+    ) {
+        _ngZone.runOutsideAngular(() => {
+            setInterval(() => {
+                this.counter++
+
+                if (this.counter % 2 === 0) {
+                    this._ngZone.run(() => {
+                        // console.log('run')
+                    })
+                    // this._cd.detectChanges();
+                }
+            }, 1000)
+        })
+        
+    }
+
+    // ngDoCheck() {
+    //     console.log('ngDoCheck')
+    // }
+
+    // ngAfterViewChecked() {
+    //     console.log('ngAfterViewChecked');
+    // }
+
+
+}
