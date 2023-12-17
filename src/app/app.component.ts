b61337748b3f4a812849045bed596145ebd3ed8f
GDER <g.debraine@easi.net>
2023-12-17 20:36:18 +0100

    test3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,62 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, NgZone, OnInit } from '@angular/core';
+
+
+interface Test {
+    count: number;
+}
+
+@Component({
+    selector: 'app-child',
+    template: `
+        <h1>{{count?.count}}</h1>
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppChildComponent {
+    @Input()
+    count!: Test | null;
+
+}
 
 @Component({
     selector: 'app-root',
     template: `
-        <h1>Hello World</h1>
-    `
+        <h1><app-child [count]="count"></app-child></h1><button (click)="onClick()">click</button>
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+    count: Test = {
+        count: 0
+    };
+
+    constructor(
+        private _ngZone: NgZone,
+        private _cd: ChangeDetectorRef
+    ) {}
+
+    ngOnInit() {
+        this.count = {
+            count: this.count.count + 1
+        }
+        this.count = {
+            count: this.count.count + 1
+        }
+        this._ngZone.run(() => {
+            setInterval(() => {
+                
+
+                this.count = {
+                    count: this.count.count + 1
+                }
+            }, 1000)
+                // this._cd.detectChanges();
+            
+        })
+        
+    }
+
+    onClick() {}
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppChildComponent, AppComponent } from './app.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        AppChildComponent
     ],
     imports: [
         BrowserModule
